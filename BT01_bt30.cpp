f0527fdef338a6846962f6a50287356e7fd45770
quocnda <122951463+quocnda@users.noreply.github.com>
2023-02-25 09:37:23 +0700

    Create BT01_bt30.cpp

---
+++ BT01_bt30.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		for(int j=n;j>=1;j--){
+			if(i<=j)cout<<"*";
+			else cout<<" ";
+		}
+		cout<<endl;
+	}
+}
