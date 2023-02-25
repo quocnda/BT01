6f30349f48ef4bc92fdd5a385b3aaa501123ba14
quocnda <122951463+quocnda@users.noreply.github.com>
2023-02-25 09:30:54 +0700

    Create BT1_b15.cpp

---
+++ BT1_b15.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string fibo(int n)
+{
+if(n==0) return "a";
+else if(n==1) return "b";
+else
+{
+return fibo(n-1)+fibo(n-2);
+}
+}
+
+int main()
+{
+for(int i=0; i<=10; i++)
+{
+cout<<fibo(i)<<endl;
+}
+return 0;
+}
